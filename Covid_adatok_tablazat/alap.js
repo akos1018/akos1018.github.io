e5b8935afe41366b8dcb42a7f9843f9d45cf6a92
akos1018 <akos.zsombor.1@debrecenibarossgabor.com>
2021-04-17 14:47:37 +0200

    Covid adatok tablazatban

---
+++ Covid_adatok_tablazat/alap.js
@@ -0,0 +1,36 @@
+function kereses(){
+    fetch("https://covid.ourworldindata.org/data/owid-covid-data.json")
+    .then(x => x.json())
+    .then(y => megjelenit(y));
+}
+
+
+
+function megjelenit(adatok){
+
+    console.log(adatok);
+
+    var orszag=document.getElementById("orszag").value;
+    var sz='<table class="table table-striped" style="width:80%; text-align:center">';
+    sz+='<tr>';
+    sz+='<th colspan="2" style="text-align:center">'+adatok[orszag].location+'</th>';
+    sz+='</tr>';
+    sz+='<tr>';
+    sz+='<th>'+'Dátum'+'</th>';
+    sz+='<th>'+'Összes eset'+'</th>';
+    sz+='</tr>';
+
+    for (elem of adatok[orszag].data){
+        sz+='<tr>';
+        sz+='<td>'+elem.date+'</td>';  
+        sz+='<td>'+elem.total_cases+'</td>'
+        sz+='</tr>';
+           
+    }
+    
+    
+    
+    document.getElementById("talalat").innerHTML=sz;
+ 
+
+}
